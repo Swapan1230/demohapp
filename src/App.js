71b221b17a4322a20424a51b2b4c6617e4107a6d
Swapan Bhowmik <Swapan1230@users.noreply.github.com>
2020-10-15 16:33:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        p > This is sample application < /p> <
-        /header>
-
-        <
-        img src = "FamilyPic.jpg"
-        alt = "logo" / >
-        <
-        /div>
+    return ( <div className = "App">
+        <header className = "App-header">
+        <p> This is sample application </p>
+        </header>
+        </div>
     );
 }
 
